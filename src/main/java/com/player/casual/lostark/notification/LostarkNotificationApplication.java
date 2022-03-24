bac812ea84db71dd6d3004c023d81f28a1a75d81
Harin3Bone <harin.thananam@gmail.com>
2022-03-24 21:43:20 +0700

    Initial project

---
+++ src/main/java/com/player/casual/lostark/notification/LostarkNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.player.casual.lostark.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostarkNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/player/casual/lostark/notification/LostarkNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.player.casual.lostark.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
